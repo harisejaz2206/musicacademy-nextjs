2cb9b99439cd5c58a962c53cf959dd506b686e80
harisejaz2206 <harisejaz2206@gmail.com>
2024-04-16 23:23:19 +0500

    Working on navbar | 16th April 2024 | 11:23 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Haris</h1>
+    <>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+        <h1 className="text-2xl text-center text-white">Hello World</h1>
+      </main>
+    </>
   );
 }
