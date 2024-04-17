4e08d0d717049b3359183db997640de2ccb5ba0d
harisejaz2206 <harisejaz2206@gmail.com>
2024-04-17 21:35:14 +0500

    Working on navbar | 17th April 2024 | 9:35 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <h1 className="text-2xl text-center text-white">Hello World</h1>
+        <HeroSection />
       </main>
     </>
   );
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='h-auto w-full md:h-[40rem] rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
+            <div className='p-4 relative z-10 w-full text-center'>
+                <h1>Master the art of music</h1>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
