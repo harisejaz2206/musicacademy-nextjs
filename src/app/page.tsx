31765ae43aefb4a8f3ffa9a0dc2b9767bd66591f
harisejaz2206 <harisejaz2206@gmail.com>
2024-04-17 23:43:52 +0500

    Working on hero section | 17th April 2024 | 11:43 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-        <h1 className="text-2xl text-center text-white">Hello World</h1>
         <HeroSection />
       </main>
     </>
